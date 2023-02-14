b3f594d53d10df03c6ce41819b78c06e9118c84a
BastienAubecq <118353870+BastienAubecq@users.noreply.github.com>
2023-02-14 19:34:30 +0100

    Add files via upload

---
+++ Robot/SocketServer.py
@@ -0,0 +1,92 @@
+import socket
+from threading import Thread
+
+class SocketServer:
+	PORT = 51399
+
+	def startServer(self, callback):
+		# Create the socket and start the server in a thread
+		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.__thread = Thread(target=lambda: self.__startServer(callback))
+		self.__thread.daemon = True
+		self.__thread.start()
+		
+	def __startServer(self, callback):
+		# Start the server and wait for the client to connect
+		self.socket.bind(("", self.PORT))
+		self.socket.listen(1)
+		self.client, address = self.socket.accept()
+		callback()
+		
+	def startReceive(self, callback):
+		# Start receiving messages in a thread
+		self.callback = callback
+		self.receiving = True
+		self.__thread = Thread(target=self.__receive)
+		self.__thread.daemon = True
+		self.__thread.start()
+		
+	def __receive(self):
+		# Receive messages and return them in the callback
+		while self.receiving:
+			message = self.client.recv(1024)
+			if(message):
+				if self.receiving:
+					self.callback(message.decode())
+			else:
+				# Client disconnected
+				self.receiving = False
+				self.stopServer()
+				
+	def stopReceive(self):
+		# Stop receiving messages
+		self.receiving = False
+			
+	def send(self, message):
+		# Send a message to the client
+		self.client.send(message.encode())
+
+	def sendBroadcast(self, message):
+		# Send a broadcast message to the network
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+		sock.sendto(message.encode(), ('<broadcast>', self.PORT))
+		sock.close()
+		
+	def stopServer(self):
+		# Stop the server
+		self.client.close()
+		self.socket.close()
+
+
+
+if __name__ == "__main__": # Test: Simple chat
+	def main():
+		while wait: pass
+		print("Connected!")
+		server.startReceive(lambda message: print(message))
+		running = True
+		while running:
+			message = input()
+			if message == "exit":
+				running = False
+				server.stopReceive()
+				server.stopServer()
+			else:
+				try:
+					server.send(message)
+				except:
+					print("Client disconnected")
+					running = False
+
+	def startMain():
+		global wait
+		wait = False
+
+	wait = True
+	server = SocketServer()
+	print("Sending broadcast...")
+	server.sendBroadcast("IP")
+	print("Waiting for client...")
+	server.startServer(startMain)
+	main()
